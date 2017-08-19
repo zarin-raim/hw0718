35309ffecba65bdec5ffe0c65ae51a01fc3387ec
zarin-raim <zarinar1105@gmail.com>
2017-08-19 20:58:05 +0600

    Maybe done

---
+++ hw/PaperPartial.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace hw
+{
+    public partial class Paper
+    {
+        public string PaperInfo()
+        {
+            return String.Format("Format: {0}\nWeight: {1}\nHeight: {2}\nWidth: {3}\nType: {4}\nColor: {5}",
+                                            InternationalType, Weight, Height, Width, Type, Color);
+        }
+        public void PaperInfo(out string[] info)
+        {
+            int size = 6;
+            info = new string[size];
+            info[1] = "Format: " + InternationalType;
+            info[2] = "Weight: " + Weight;
+            info[3] = "Height: " + Height;
+            info[4] = "Width: " + Width;
+            info[5] = "Type: " + Type;
+            info[6] = "Color: " + Color;
+        }
+    }
+}
--- hw/Program.cs
+++ hw/Program.cs
@@ -9,6 +9,23 @@ namespace hw
     {
         static void Main(string[] args)
         {
+            Paper[] papers = new Paper[5];
+
+            papers[0] = new Paper(210, "A2");
+            papers[1] = new Paper(80, "A4");
+            papers[2] = new Paper(120, "A4");
+            papers[3] = new Paper(80, "A5");
+            papers[4] = new Paper(60, "A3");
+
+            for (int i = 0; i < papers.Length; i++)
+            {
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine("[" + i + "]\n" + papers[i].PaperInfo());
+                Console.WriteLine("-----------------------------------");
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
